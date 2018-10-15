b08b3e2932a539090348badef03f5b1bd3380ec5
Sheenan Tenepre <sheenantenepre@gmail.com>
2018-10-16 01:15:32 +0800

    update like routes

--- src/routes/api/like.ts
+++ src/routes/api/like.ts
@@ -6,17 +6,24 @@ import * as like from "../../controllers/like";
 const router = Router();
 
 // like a post
-router.post(
-  "/:postId",
-  auth.required,
-  like.likePost
-);
+// router.post(
+//   "/:postId",
+//   auth.required,
+//   like.likePost
+// );
 
 // unlike a post
 router.delete(
-  "/:likeId",
+  "/",
   auth.required,
   like.unlikePost
+);
+
+// like or unlike a post
+router.post(
+  "/",
+  auth.required,
+  like.likeUnlikePost
 );
 
 export default router;
